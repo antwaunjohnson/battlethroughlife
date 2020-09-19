acc96b90110d3cb0a09bb4bc0dfb5a57498ea6d9
Antwaun Johnson <antwaun.l.johnson@gmail.com>
2020-09-19 07:27:08 -0700

    removed datetime import

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+
 
 
 app = Flask(__name__)
