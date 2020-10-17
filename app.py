9f9bded78a0ebb3c34093aa6c408ca1503b5c79d
Antwaun Johnson <antwaun.l.johnson@gmail.com>
2020-10-17 10:27:21 -0700

    removed tags table

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-from datetime import datetime
-from os import environ
-
 
 app = Flask(__name__)
 
@@ -11,7 +8,7 @@ app = Flask(__name__)
 
 
 
-ENV = 'prod'
+ENV = 'dev'
 
 if ENV == 'dev':
     app.debug = True
@@ -21,8 +18,7 @@ else:
     app.debug = False
     app.config['SQLALCHEMY_DATABASE_URI'] = 'DATABASE_URL'
 
-    app.config['SECRET_KEY'] = environ.get('SECRET_KEY')
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
@@ -36,40 +32,15 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String(100), unique=False)
     content = db.Column(db.Text, unique=False)
-    featured_image = db.Column(db.String, nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    
+   
 
-    def __init__(self, id, title, content, featured_image, created_at):
-        self.id = id
+    def __init__(self, title, content):
         self.title = title
         self.content = content
-        self.featured_image = featured_image
-        self.created_at = created_at
-
-# Tag model 
-
-
-class Tag(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20))
-
-    def __init__(self, id, name):
-        self.id = id
-        self.name = name
-
-# Tag_blog table
-
-tag_blog = db.Table('tag_blog',
-    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'), primary_key=True),
-    db.Column('blog_id', db.Integer, db.ForeignKey('blog.id'), primary_key=True)
-)
-
-# Blog schema and routes for api
 
 class BlogSchema(ma.Schema):
     class Meta:
-        fields = (id, 'title', 'content', 'featured_image', 'created_at')
+        fields = ('title', 'content')
 
 blog_schema = BlogSchema()
 blogs_schema = BlogSchema(many=True)
@@ -78,23 +49,10 @@ blogs_schema = BlogSchema(many=True)
 
 @app.route('/add_blog', methods=['POST'])
 def add_blog():
-    data = request.get_json()
-
     title = request.json['title']
     content = request.json['content']
-    featured_image = request.json['featured_image']
-    
 
-    new_blog = Blog( title, content, featured_image)
-
-    for tag in data['tags']:
-        present_tag = Tag.query.filter_by(name=tag).first()
-        if(present_tag):
-            present_tag.blogs_associated.append(new_blog)
-        else:
-            new_tag=Tag(name=tag)
-            new_tag.blogs_associated.append(new_blog)
-            db.session.add(new_tag)
+    new_blog = Blog(title, content)
 
     db.session.add(new_blog)
     db.session.commit()
@@ -116,10 +74,6 @@ def get_blogs():
 @app.route('/blog/<id>', methods=["GET"])
 def get_blog(id):
     blog = Blog.query.get(id)
-
-    for tag in blog.tags:
-        tag.append(tag)
-
     return blog_schema.jsonify(blog)
 
 
@@ -129,18 +83,16 @@ def update_blog(id):
     blog = Blog.query.get(id)
     title = request.json['title']
     content = request.json['content']
-    featured_image = request.json['featured_image']
 
     blog.title = title
     blog.content = content
-    blog.featured_image = featured_image
 
     db.session.commit()
     return blog_schema.jsonify(blog)
 
 
 #Endpoint for deleting a blog
-@app.route('/delete_blog/<id>', methods=["DELETE"])
+@app.route('/blog/<id>', methods=["DELETE"])
 def delete_blog(id):
     blog = Blog.query.get(id)
     title = request.json['title']
